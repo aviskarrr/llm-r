931a255fe415721a95a23fd96991c0a980c11892
3PI70M3 <74763133+aviskarrr@users.noreply.github.com>
2025-01-27 20:23:16 +0545

    changes

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-const express = require('express');
-const app = express();
+// fileName : server.js 
+// Example using the http module
+const http = require('http');
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+    // Set the response headers
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    // Write the response content
+    res.write('<h1>Hello, Node.js HTTP Server!</h1>');
+    res.end();
+});
+
+
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World! byeeee');
+// Start the server
+server.listen(port, () => {
+    console.log(`Node.js HTTP server is running on port ${port}`);
 });
-
-app.listen(port, () => {
-    console.log(`listening at http://localhost:${port}`);
-});
-
